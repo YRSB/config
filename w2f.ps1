f377100058a144411ac8a5e8ce99c676272bff07
Tsui no Sora <32013347+YRSB@users.noreply.github.com>
2024-08-28 15:47:04 +0800

    v0.1

--- w2f.ps1
+++ w2f.ps1
@@ -5,7 +5,6 @@ $lrc_files = Get-ChildItem -Filter *.lrc
 $vtt_files = Get-ChildItem | Where-Object {$_.FullName -match "\w+\.(txt|vtt)$"}
 
 # create destination dir if not exists
-
 $folders=Get-ChildItem -Directory
 if(-not (Test-Path .\output)){
     mkdir output
@@ -40,37 +39,23 @@ if(($lrc_files.Count -eq 0) -and ($vtt_files.Count -eq 0)){
             $output=$file.BaseName+lrc
             ffmpeg -hide_banner -i $file.Name .\output\$output
         }
+    }else{
+        Copy-Item *.lrc .\output\
     }
-    # update lrc_files
-    $lrc_files = Get-ChildItem .\output -Filter *.lrc
 }
 
 Set-Location output
-
-
-# 遍历指定目录中的所有 .flac 文件
-Get-ChildItem -Filter *.flac | ForEach-Object {
-    $flac = $_
-    # *.flac.lrc
-    $lrc1 = $flac.Name+".lrc"
-    # *.lrc
-    $lrc2 = [System.IO.Path]::ChangeExtension($flac.Name, ".lrc")
-    # 检查是否存在对应的 .lrc 文件
-    # 情况1：a.flac 和 a.flac.lrc，此时$flac.Name和$lrc.BaseName比
-    # 情况2：a.flac 和 a.lrc
-
-    if (Test-Path $lrc) {
-        $lyricsContent = Get-Content $lrcFilePath -Raw
-        
-        # 确保 metaflac 工具可用
+$lrc_files = Get-ChildItem . -Filter *.lrc
+$ext = ".flac"
+foreach($lrc in $lrc_files){
+    $lrc_name = $lrc.Name.Split('.')[0]
+    if(Test-Path ($lrc_name+$ext)){
+        $lrcContent = Get-Content $lrc -Encoding UTF8
         if (Get-Command metaflac -ErrorAction SilentlyContinue) {
-            # 使用 metaflac 添加 lyrics 标签
-            & metaflac --set-tag="LYRICS=$lyricsContent" $flacFile.FullName
-            Write-Host "歌词已嵌入到文件: $($flacFile.FullName)"
+            metaflac --set-tag="LYRICS=$lrcContent" $lrc_name.flac
+            Write-Host "$()已嵌入到文件: $($lrc_name.flac)"
         } else {
             Write-Error "metaflac 工具未找到，请确保它已安装并在系统路径中。"
         }
-    } else {
-        Write-Warning "找不到对应的 lrc 文件: $lrcFilePath"
     }
 }
